a29b1270874e659892659fdf310f2784cbb90757
zelindro <zelindroarthur@gmail.com>
2024-11-19 09:59:52 -0300

    feat: handle producer page

--- admin/src/app/produtores/page.tsx
+++ admin/src/app/produtores/page.tsx
@@ -1,5 +1,13 @@
 import Title from "../components/Title";
 
 export default function page() {
-  return <Title>Aprovar produtores</Title>;
+  return (
+    <div className="h-full w-full bg-white">
+      <Title>Produtores</Title>
+      <div className="h-full w-full">
+        {/* <Card> */}
+      </div>
+    </div>
+    
+  );
 }
