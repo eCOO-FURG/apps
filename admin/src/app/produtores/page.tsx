a9275e5bd80639d9deae0ae4c4f58735636ffeba
zelindro <zelindroarthur@gmail.com>
2024-11-21 17:14:56 -0300

    feat: producer table mocked

--- admin/src/app/produtores/page.tsx
+++ admin/src/app/produtores/page.tsx
@@ -1,13 +1,39 @@
+'use client';
+
+import { useState } from "react";
+
+import ProducerTable from "../components/ProducerTable";
 import Title from "../components/Title";
 
+import { useDebounce } from "@shared/hooks/useDebounce";
+import SearchInput from "@shared/components/SearchInput";
+
 export default function page() {
+  const [name, setName] = useState("");
+  const debounceSearch = useDebounce(name);
+
   return (
-    <div className="h-full w-full bg-white">
-      <Title>Produtores</Title>
-      <div className="h-full w-full">
-        {/* <Card> */}
+    // <div className="flex gap-8">
+    //   <div className="text-white h-96 bg-yellow-500 flex flex-auto items-center justify-center">
+    //     Hello 1
+    //   </div>
+    //   <div className="text-white h-96 bg-blue-500 flex flex-auto items-center justify-center">
+    //     Hello 2
+    //   </div>
+    //   <div className="text-white h-96 bg-red-500 flex flex-auto items-center justify-center">
+    //     Hello 3
+    //   </div>
+    // </div>
+    <div className="w-full flex flex-col h-full gap-5">
+      <div className="flex w-full items-center justify-between">
+        <Title>Produtores</Title>
+        <div className="w-2/5">
+          <SearchInput onChange={setName} />
+        </div>
+      </div>
+      <div className="rounded-xl bg-white">
+        <ProducerTable name={debounceSearch} />
       </div>
     </div>
-    
   );
 }
