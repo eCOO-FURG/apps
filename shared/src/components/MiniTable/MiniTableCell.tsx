84b581c297b76b37a11d08908c267d3e116b4798
Murilo Vargas <muriraubach@gmail.com>
2025-04-19 20:23:39 -0300

    Enhance MiniTableCell component by adding optional className prop for improved styling flexibility

--- shared/src/components/MiniTable/MiniTableCell.tsx
+++ shared/src/components/MiniTable/MiniTableCell.tsx
@@ -3,11 +3,13 @@ import { twMerge } from "tailwind-merge";
 
 interface MiniTableCellProps
   extends React.TdHTMLAttributes<HTMLTableCellElement> {
+  className?: string;
   children: ReactNode;
 }
 
 export default function MiniTableCell({
   children,
+  className,
   ...rest
 }: MiniTableCellProps) {
   return (
